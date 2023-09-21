b20772cd16da7aa88a40ec09c0ff823632ec2921
Adam Davies <adam.davies@version1.com>
2023-09-21 10:54:43 +0100

    Fixes for markAsComplete

--- runner/src/server/plugins/engine/models/viewModel.ts
+++ runner/src/server/plugins/engine/models/viewModel.ts
@@ -68,7 +68,6 @@ export class ViewModel {
     const { def } = model;
     // @ts-ignore
     this.declaration = def.declaration;
-    this.markAsComplete = def.markAsComplete;
     // @ts-ignore
     this.skipSummary = def.skipSummary;
     this._payApiKey = def.payApiKey;
