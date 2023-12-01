1f3e4378813576f0e4fddeb1a80ae47bccfce237
AbuNavsa <60141291+AbuNavsa@users.noreply.github.com>
2023-12-01 10:58:38 +0000

    edited test

--- runner/test/cases/server/plugins/engine/clientsidefileupload.test.ts
+++ runner/test/cases/server/plugins/engine/clientsidefileupload.test.ts
@@ -1,7 +1,6 @@
 import * as Code from "@hapi/code";
 import * as Lab from "@hapi/lab";
 import { ClientSideFileUploadField } from "src/server/plugins/engine/components";
-import { validationOptions } from "../../../../../src/server/plugins/engine/pageControllers/validationOptions";
 
 const lab = Lab.script();
 exports.lab = lab;
@@ -39,34 +38,5 @@ suite("ClientSideFileUploadField", () => {
       showNoScriptWarning: false,
       minimumRequiredFiles: 1,
     });
-  });
-
-  test("Should supply custom validation message if defined", () => {
-    const def = {
-      name: "myFileUpload",
-      title: "My File Upload",
-      hint: "a hint",
-      options: {
-        dropzoneConfig: {
-          maxFiles: 1,
-          parallelUploads: 1,
-          maxFilesize: 5,
-          acceptedFiles:
-            "image/jpeg,image/png,application/pdf,text/plain,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.oasis.opendocument.text,text/csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.oasis.opendocument.spreadsheet",
-        },
-        showNoScriptWarning: false,
-        minimumRequiredFiles: 1,
-        customValidationMessage: "This is a custom error",
-      },
-      schema: {},
-    };
-    const fileUploadField = new ClientSideFileUploadField(def, {});
-    const { validation } = fileUploadField;
-
-    expect(validation("valid_file_path", validationOptions).error).to.be.undefined();
-
-    expect(
-      validation("invalid_file_path", validationOptions).error.message
-    ).to.equal("This is a custom error");
   });
 });
